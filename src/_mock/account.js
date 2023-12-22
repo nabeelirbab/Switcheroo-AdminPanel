86bf0771ab6533f7ebee71a4ce368ca9b27d612a
tayyabraja <tayyabraja316@gmail.com>
2023-12-22 18:01:16 +0500

    initial commit

---
+++ src/_mock/account.js
@@ -0,0 +1,7 @@
+// ----------------------------------------------------------------------
+
+export const account = {
+  displayName: 'Tayyab Raja',
+  email: 'adminpanel@swircheroo.com',
+  photoURL: '/assets/images/avatars/avatar_25.jpg',
+};
---
+++ src/layouts/dashboard/config-navigation.jsx
@@ -0,0 +1,37 @@
+import SvgColor from 'src/components/svg-color';
+
+// ----------------------------------------------------------------------
+
+const icon = (name) => (
+  <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />
+);
+
+const navConfig = [
+  {
+    title: 'dashboard',
+    path: '/',
+    icon: icon('ic_analytics'),
+  },
+  {
+    title: 'user',
+    path: '/user',
+    icon: icon('ic_user'),
+  },
+  {
+    title: 'reported products',
+    path: '/reported-products',  
+    icon: icon('ic_cart'),
+  },
+  {
+    title: 'reported user',
+    path: '/reported-user',
+    icon: icon('ic_blog'),
+  },
+  // {
+  //   title: 'login',
+  //   path: '/login',
+  //   icon: icon('ic_lock'),
+  // },
+];
+
+export default navConfig;
