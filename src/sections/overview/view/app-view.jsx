93a495f8331ae122dc2e78f6d409683bbb3c22d4
tayyabraja <tayyabraja316@gmail.com>
2024-03-17 01:11:57 +0500

    user,item and offer engagements charts added

--- src/sections/overview/view/app-view.jsx
+++ src/sections/overview/view/app-view.jsx
@@ -1,12 +1,24 @@
+import React, { useState } from 'react';
 import { gql, useQuery } from '@apollo/client';
+import {
+  Line,
+  XAxis,
+  YAxis,
+  Legend,
+  Tooltip,
+  LineChart,
+  CartesianGrid,
+  ResponsiveContainer,
+} from 'recharts';
 
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Unstable_Grid2';
 import Typography from '@mui/material/Typography';
+import { Select, MenuItem, FormControl } from '@mui/material';
+import CircularProgress from '@mui/material/CircularProgress';
 
 import GenderCount from '../gender-count';
 import AppWidgetSummary from '../app-widget-summary';
-// import UserEngagementGraph from '../user-engagement';
 import CategoriesItemCount from '../categories-item-count';
 
 // ----------------------------------------------------------------------
@@ -73,29 +85,124 @@ const TOTAL_ITEMS = gql`
   }
 `;
 
+const USER_ENGAGEMENTS = gql`
+  query UserEngagement {
+    userEngagement {
+      dailyRegistrationTrend {
+        count
+        date
+      }
+      monthlyRegistrationTrend {
+        count
+        month
+        year
+      }
+      weeklyRegistrationTrend {
+        count
+        week
+        year
+      }
+    }
+  }
+`;
+
+const ITEM_ENGAGEMENTS = gql`
+  query ItemEngagement {
+    itemEngagement {
+      dailyRegistrationTrend {
+        count
+        date
+      }
+      monthlyRegistrationTrend {
+        count
+        month
+        year
+      }
+      weeklyRegistrationTrend {
+        count
+        week
+        year
+      }
+    }
+  }
+`;
+
+const OFFER_ENGAGEMENTS = gql`
+  query OfferEngagement {
+    offerEngagement {
+      countByType {
+        acceptedCashOffers
+        cashOffers
+        matchedOffers
+        unMatchedOffers
+      }
+      dailyTrend {
+        count
+        date
+      }
+      monthlyTrend {
+        count
+        month
+        year
+      }
+      weeklyTrend {
+        count
+        week
+        year
+      }
+    }
+  }
+`;
+
 export default function AppView() {
+  const [selectedOption, setSelectedOption] = useState('daily');
+  const [selectedOptionItem, setSelectedOptionItem] = useState('daily');
+  const [selectedOptionOffer, setSelectedOptionOffer] = useState('daily');
+
   const { data: userdata, loading: userLoading, error: userError } = useQuery(GET_ALL_USERS);
   const {
     data: totalitemsdata,
     loading: totalitemsLoading,
     error: totalitemsError,
   } = useQuery(TOTAL_ITEMS);
+
   const {
     data: restricteduserdata,
     loading: restrictedUserLoading,
     error: restrictedUserError,
   } = useQuery(GET_REPORTED_USERS);
+
   const {
     data: ProductsCountdata,
     loading: productsLoading,
     error: productsError,
   } = useQuery(RESTRICTED_PRODUCTS);
+
   const { loading, error, data } = useQuery(GENDER_COUNT);
+
   const {
     loading: itemloading,
     error: itemerror,
     data: categoriesItemData,
   } = useQuery(CATEGORIES_ITEM_COUNT);
+
+  const {
+    loading: userEngagementloading,
+    error: userEngagementerror,
+    data: userEngagementData,
+  } = useQuery(USER_ENGAGEMENTS);
+
+  const {
+    loading: itemEngagementloading,
+    error: itemEngagementerror,
+    data: itemEngagementData,
+  } = useQuery(ITEM_ENGAGEMENTS);
+
+  const {
+    loading: offerEngagementloading,
+    error: offerEngagementerror,
+    data: offerEngagementData,
+  } = useQuery(OFFER_ENGAGEMENTS);
 
   if (
     loading ||
@@ -103,10 +210,29 @@ export default function AppView() {
     productsLoading ||
     itemloading ||
     restrictedUserLoading ||
-    totalitemsLoading
+    totalitemsLoading ||
+    userEngagementloading ||
+    itemEngagementloading ||
+    offerEngagementloading
   )
-    return <p>Loading...</p>;
-  if (error || userError || productsError || itemerror || restrictedUserError || totalitemsError)
+    return (
+      <div
+        style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}
+      >
+        <CircularProgress />
+      </div>
+    );
+  if (
+    error ||
+    userError ||
+    productsError ||
+    itemerror ||
+    restrictedUserError ||
+    totalitemsError ||
+    userEngagementerror ||
+    itemEngagementerror ||
+    offerEngagementerror
+  )
     return (
       <p>
         Error:{' '}
@@ -115,7 +241,10 @@ export default function AppView() {
           itemerror?.message ||
           productsError?.message ||
           restrictedUserError?.message ||
-          totalitemsError.message}
+          totalitemsError?.message ||
+          userEngagementerror?.message ||
+          itemEngagementerror?.message ||
+          offerEngagementerror?.message}
       </p>
     );
 
@@ -131,13 +260,13 @@ export default function AppView() {
     : 0;
 
   const RestrictedUsersCount = restricteduserdata?.restrictedUsers
-  ? restricteduserdata.restrictedUsers.reduce((count, product) => {
-      if (product.targetUser && product.targetUser.length > 0) {
-        return count + 1;
-      }
-      return count;
-    }, 0)
-  : 0;
+    ? restricteduserdata.restrictedUsers.reduce((count, product) => {
+        if (product.targetUser && product.targetUser.length > 0) {
+          return count + 1;
+        }
+        return count;
+      }, 0)
+    : 0;
 
   const TotalItemsCount = totalitemsdata.allItemsInDatabase.totalCount;
 
@@ -151,6 +280,214 @@ export default function AppView() {
     label: item.key,
     value: item.value,
   }));
+
+  const userEngagement = userEngagementData;
+  const itemEngagement = itemEngagementData;
+  const offerEngagement = offerEngagementData;
+  console.log(offerEngagement, 'offer engagement');
+
+  // User Engagement data for daily, monthly, and weekly engagement
+  const dailyData = userEngagement.userEngagement.dailyRegistrationTrend.map((item) => ({
+    date: item.date.split('T')[0],
+    count: item.count,
+  }));
+
+  const monthlyData = userEngagement.userEngagement.monthlyRegistrationTrend.map((item) => ({
+    label: `${item.month}/${item.year}`,
+    count: item.count,
+  }));
+
+  const weeklyData = userEngagement.userEngagement.weeklyRegistrationTrend.map((item) => ({
+    label: `${item.week} week/${item.year}`,
+    count: item.count,
+  }));
+
+  // Item Engagement data for daily, monthly, and weekly engagement
+
+  const dailyItemData = itemEngagement.itemEngagement.dailyRegistrationTrend.map((item) => ({
+    date: item.date.split('T')[0],
+    count: item.count,
+  }));
+
+  const monthlyItemData = itemEngagement.itemEngagement.monthlyRegistrationTrend.map((item) => ({
+    label: `${item.month}/${item.year}`,
+    count: item.count,
+  }));
+
+  const weeklyItemData = itemEngagement.itemEngagement.weeklyRegistrationTrend.map((item) => ({
+    label: `${item.week} week/${item.year}`,
+    count: item.count,
+  }));
+
+  // Offer Engagement data for daily, monthly, and weekly engagement
+
+  const dailyOfferData = offerEngagement.offerEngagement.dailyTrend.map((item) => ({
+    date: item.date.split('T')[0],
+    count: item.count,
+  }));
+
+  const monthlyOfferData = offerEngagement.offerEngagement.monthlyTrend.map((item) => ({
+    label: `${item.month}/${item.year}`,
+    count: item.count,
+  }));
+
+  const weeklyOfferData = offerEngagement.offerEngagement.weeklyTrend.map((item) => ({
+    label: `${item.week} week/${item.year}`,
+    count: item.count,
+  }));
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  const handleItemOptionChange = (event) => {
+    setSelectedOptionItem(event.target.value);
+  };
+
+  const handleOfferOptionChange = (event) => {
+    setSelectedOptionOffer(event.target.value);
+  };
+
+  const renderChart = () => {
+    switch (selectedOption) {
+      case 'daily':
+        return (
+          <ResponsiveContainer width="100%" height={400}>
+            <LineChart data={dailyData}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="date" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="count" stroke="#1877f2" strokeWidth={4} />
+            </LineChart>
+          </ResponsiveContainer>
+        );
+
+      case 'weekly':
+        return (
+          <ResponsiveContainer width="100%" height={400}>
+            <LineChart data={weeklyData}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="label" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="count" stroke="#ffc658" strokeWidth={4} />
+            </LineChart>
+          </ResponsiveContainer>
+        );
+      case 'monthly':
+        return (
+          <ResponsiveContainer width="100%" height={400}>
+            <LineChart data={monthlyData}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="label" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="count" stroke="#82ca9d" strokeWidth={4} />
+            </LineChart>
+          </ResponsiveContainer>
+        );
+      default:
+        return null;
+    }
+  };
+
+  const renderChartItemEngagement = () => {
+    switch (selectedOptionItem) {
+      case 'daily':
+        return (
+          <ResponsiveContainer width="100%" height={400}>
+            <LineChart data={dailyItemData}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="date" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="count" stroke="#8884d8" strokeWidth={4} />
+            </LineChart>
+          </ResponsiveContainer>
+        );
+
+      case 'weekly':
+        return (
+          <ResponsiveContainer width="100%" height={400}>
+            <LineChart data={weeklyItemData}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="label" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="count" stroke="#f05b5b" strokeWidth={4} />
+            </LineChart>
+          </ResponsiveContainer>
+        );
+      case 'monthly':
+        return (
+          <ResponsiveContainer width="100%" height={400}>
+            <LineChart data={monthlyItemData}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="label" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="count" stroke="#d0f05b" strokeWidth={4} />
+            </LineChart>
+          </ResponsiveContainer>
+        );
+      default:
+        return null;
+    }
+  };
+
+  const renderChartOfferEngagement = () => {
+    switch (selectedOptionOffer) {
+      case 'daily':
+        return (
+          <ResponsiveContainer width="100%" height={400}>
+            <LineChart data={dailyOfferData}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="date" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="count" stroke="#2fe4eb" strokeWidth={4} />
+            </LineChart>
+          </ResponsiveContainer>
+        );
+
+      case 'weekly':
+        return (
+          <ResponsiveContainer width="100%" height={400}>
+            <LineChart data={weeklyOfferData}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="label" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="count" stroke="#2feb7d" strokeWidth={4} />
+            </LineChart>
+          </ResponsiveContainer>
+        );
+      case 'monthly':
+        return (
+          <ResponsiveContainer width="100%" height={400}>
+            <LineChart data={monthlyOfferData}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="label" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="count" stroke="#f071e1" strokeWidth={4} />
+            </LineChart>
+          </ResponsiveContainer>
+        );
+      default:
+        return null;
+    }
+  };
 
   return (
     <Container maxWidth="xl">
@@ -214,9 +551,74 @@ export default function AppView() {
         </Grid>
       </Grid>
 
-      {/* <Grid xs={6} sm={6} md={3}>
-        <UserEngagementGraph chartData={chartData} />
-      </Grid> */}
+      <Container
+        style={{
+          backgroundColor: 'white',
+          borderRadius: '10px',
+          padding: '20px',
+          boxShadow:
+            '0 0 2px 0 rgba(145, 158, 171, 0.08), 0 12px 24px -4px rgba(145, 158, 171, 0.08)',
+          marginTop: '20px',
+        }}
+      >
+        <FormControl variant="outlined" sx={{ m: 2 }}>
+          <Typography sx={{ marginBottom: '10px' }} variant="h6">
+            User Engagements{' '}
+          </Typography>
+          <Select value={selectedOption} onChange={handleOptionChange}>
+            <MenuItem value="daily">Daily</MenuItem>
+            <MenuItem value="weekly">Weekly</MenuItem>
+            <MenuItem value="monthly">Monthly</MenuItem>
+          </Select>
+        </FormControl>
+        {renderChart()}
+      </Container>
+
+      <Container
+        style={{
+          backgroundColor: 'white',
+          borderRadius: '10px',
+          padding: '20px',
+          boxShadow:
+            '0 0 2px 0 rgba(145, 158, 171, 0.08), 0 12px 24px -4px rgba(145, 158, 171, 0.08)',
+          marginTop: '20px',
+        }}
+      >
+        <FormControl variant="outlined" sx={{ m: 2 }}>
+          <Typography sx={{ marginBottom: '10px' }} variant="h6">
+            Item Engagements{' '}
+          </Typography>
+          <Select value={selectedOptionItem} onChange={handleItemOptionChange}>
+            <MenuItem value="daily">Daily</MenuItem>
+            <MenuItem value="weekly">Weekly</MenuItem>
+            <MenuItem value="monthly">Monthly</MenuItem>
+          </Select>
+        </FormControl>
+        {renderChartItemEngagement()}
+      </Container>
+
+      <Container
+        style={{
+          backgroundColor: 'white',
+          borderRadius: '10px',
+          padding: '20px',
+          boxShadow:
+            '0 0 2px 0 rgba(145, 158, 171, 0.08), 0 12px 24px -4px rgba(145, 158, 171, 0.08)',
+          marginTop: '20px',
+        }}
+      >
+        <FormControl variant="outlined" sx={{ m: 2 }}>
+          <Typography sx={{ marginBottom: '10px' }} variant="h6">
+            Offer Engagements{' '}
+          </Typography>
+          <Select value={selectedOptionOffer} onChange={handleOfferOptionChange}>
+            <MenuItem value="daily">Daily</MenuItem>
+            <MenuItem value="weekly">Weekly</MenuItem>
+            <MenuItem value="monthly">Monthly</MenuItem>
+          </Select>
+        </FormControl>
+        {renderChartOfferEngagement()}
+      </Container>
     </Container>
   );
 }
